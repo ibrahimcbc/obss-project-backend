3c7e6b5b22299717b0ee5dfa95ba075dd3cba12f
brahim Cebecioğlu <icebecioglu21@ku.edu.tr>
2024-08-10 12:40:48 +0300

    first commit

---
+++ src/main/java/org/example/obssfinalproject/ObssFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.obssfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObssFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObssFinalProjectApplication.class, args);
+    }
+
+}
