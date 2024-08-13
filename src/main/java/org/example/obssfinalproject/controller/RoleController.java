38f8bde1391e1a35e5d02e359097d3692a362d6a
Mustafa Tetik <tetikmustafa03@gmail.com>
2024-08-13 21:12:56 +0300

    @RequestMapping("/api/roles")

--- src/main/java/org/example/obssfinalproject/controller/RoleController.java
+++ src/main/java/org/example/obssfinalproject/controller/RoleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/roles")
 public class RoleController {
 
     @Autowired
